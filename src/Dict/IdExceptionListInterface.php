70c0adbf3f73e38900f66894012248f1619ad32c
Abecek <abcq12013@gmail.com>
2025-08-14 00:17:21 +0200

    extended exception id list

--- src/Dict/IdExceptionListInterface.php
+++ src/Dict/IdExceptionListInterface.php
@@ -6,8 +6,10 @@ namespace MapMissingItems\Dict;
 interface IdExceptionListInterface
 {
     public const array IDS_TO_SKIP = [
-        13031,
-        8046,
-        8047,
+        13031, // nothing, blocking walking item
+        8046, // something sparkling
+        8047, // something sparkling
+        8029, // nothing
+        7288, // nothing, prevent moving item below it (suspicion)
     ];
 }
